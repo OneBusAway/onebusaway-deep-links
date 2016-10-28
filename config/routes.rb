956ee7a94cf0b8a9afc6b78e0f8eb3be35ff8b87
Aaron Brethorst <aaron@brethorsting.com>
2016-10-27 23:17:43 -0700

    Super-basic actions for regions#index and regions#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'regions#index'
+
   resources :regions, only: [:index, :show]
   get 'apple-app-site-association', to: 'regions#site_association', defaults: { format: 'json' }
 end
