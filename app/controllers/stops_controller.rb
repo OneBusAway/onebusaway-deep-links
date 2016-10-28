ca27e8528df330fe67d2993d62cf67c0fb473723
Aaron Brethorst <aaron@brethorsting.com>
2016-10-28 12:21:00 -0700

    Add a note that New York doesn't seem to be feasible at present

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -4,10 +4,12 @@ class StopsController < ApplicationController
     @region = Region.find(params[:region_id])
 
     # TODO: needs special handling
-    # MTA New York - Region API ID 2
     # Washington DC - Region API ID: 4
     # York - Region API ID: 5
     # Bear Transit - Region API ID: 6
+
+    # Not handled currently:
+    # MTA New York - Region API ID 2
 
     redirect_to File.join(@region.web_url, "/where/standard/stop.action?id=#{params[:id]}")
   end
