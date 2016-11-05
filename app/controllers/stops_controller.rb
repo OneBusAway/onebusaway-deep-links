516f4b96ea9a6b0e13e56ec0d5632ee196009be1
Aaron Brethorst <aaron@brethorsting.com>
2016-11-05 10:54:42 -0700

    remove comments that have been turned into issues

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -2,15 +2,6 @@ class StopsController < ApplicationController
 
   def show
     @region = Region.find_by(region_identifier: params[:region_id])
-
-    # TODO: needs special handling
-    # Washington DC - Region API ID: 4
-    # York - Region API ID: 5
-    # Bear Transit - Region API ID: 6
-
-    # Not handled currently:
-    # MTA New York - Region API ID 2
-
     redirect_to File.join(@region.web_url, "/where/standard/stop.action?id=#{params[:id]}")
   end
 
