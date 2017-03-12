f4c8d4a68b2e142b0ac4c8fb3ef93acde3c418d3
Ben M <bmenesini@gmail.com>
2017-03-12 13:31:30 -0700

    Moves AlertFeedsController#show to a more RESTful endpoint
    `AlertFeedsController#show` was previously at `/regions/:region_id/alert_feeds/:id`, which has unnecessary nesting. Now, `AlertFeedsController#show` is at `/alert_feeds/:id`.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'regions#index'
 
+  resources :alert_feeds, only: [:show]
   resources :regions, only: [:index, :show] do
-    resources :alert_feeds, only: [:index, :show]
+    resources :alert_feeds, only: [:index]
     resources :alert_feed_items, only: [:index]
     resources :stops, only: [:show] do
       get 'arrivals'
