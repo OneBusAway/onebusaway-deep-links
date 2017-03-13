2d0c4a17ba402d44228a54b038bdf631c19c2030
Aaron Brethorst <aaron@brethorsting.com>
2017-03-13 00:46:50 -0700

    Limits returned alert feed items count to 20
    Apply a limit to the number of returned feed items so that we don’t send hundreds of alerts to the user the first time they download the list

--- app/controllers/alert_feed_items_controller.rb
+++ app/controllers/alert_feed_items_controller.rb
@@ -3,7 +3,7 @@ class AlertFeedItemsController < ApplicationController
 
   def index
     region = Region.find_by(region_identifier: params[:region_id])
-    items = region.alert_feed_items.where(condition_filters(params))
+    items = region.alert_feed_items.where(condition_filters(params)).limit(20)
     render json: items.to_json
   end
 end
