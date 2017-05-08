c00812f46ac08428e1935df812e80d6732ba5fc5
Aaron Brethorst <aaron@brethorsting.com>
2017-05-07 19:13:49 -0700

    Temporarily disables alert feed items

--- app/controllers/alert_feed_items_controller.rb
+++ app/controllers/alert_feed_items_controller.rb
@@ -2,10 +2,11 @@ class AlertFeedItemsController < ApplicationController
   include AlertFeedItemsConcerns
 
   def index
-    region = Region.find_by(region_identifier: params[:region_id])
-    @items = region.alert_feed_items.includes(:alert_feed).where(condition_filters(params)).limit(20)
-    respond_to do |format|
-      format.json
-    end
+    # region = Region.find_by(region_identifier: params[:region_id])
+    # @items = region.alert_feed_items.includes(:alert_feed).where(condition_filters(params)).limit(20)
+    # respond_to do |format|
+    #   format.json
+    # end
+    render json: []
   end
 end
