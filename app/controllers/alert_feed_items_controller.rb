4d7bc4548668af1989f0dcb5ecfe98086495e624
Aaron Brethorst <aaron@brethorsting.com>
2017-10-28 11:16:08 -0700

    Enables alert feed items, disables automatic creation of them

--- app/controllers/alert_feed_items_controller.rb
+++ app/controllers/alert_feed_items_controller.rb
@@ -2,11 +2,10 @@ class AlertFeedItemsController < ApplicationController
   include AlertFeedItemsConcerns
 
   def index
-    # region = Region.find_by(region_identifier: params[:region_id])
-    # @items = region.alert_feed_items.includes(:alert_feed).where(condition_filters(params)).limit(20)
-    # respond_to do |format|
-    #   format.json
-    # end
-    render json: []
+    region = Region.find_by(region_identifier: params[:region_id])
+    @items = region.alert_feed_items.includes(:alert_feed).where(condition_filters(params)).limit(20)
+    respond_to do |format|
+      format.json
+    end
   end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,9 +1,9 @@
 desc 'This task is called by the Heroku scheduler add-on'
 task :update_alert_feeds => :environment do
-  puts 'Updating alert feeds'
-  AlertFeed.find_each do |feed|
-    puts "Updating alert feed: \"#{feed.name}\""
-    feed.fetch_if_necessary
-  end
-  puts 'Done updating alert feeds'
+  # puts 'Updating alert feeds'
+  # AlertFeed.find_each do |feed|
+  #   puts "Updating alert feed: \"#{feed.name}\""
+  #   feed.fetch_if_necessary
+  # end
+  # puts 'Done updating alert feeds'
 end
