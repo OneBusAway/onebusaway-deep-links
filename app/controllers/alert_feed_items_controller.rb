be562faad99b35b877d6b1ce09c639a643725cd2
Aaron Brethorst <aaron@brethorsting.com>
2017-10-28 11:48:01 -0700

    Fixes bug where unknown regions were passed in to the alerts controller, causing a 500 error

--- app/controllers/alert_feed_items_controller.rb
+++ app/controllers/alert_feed_items_controller.rb
@@ -3,7 +3,12 @@ class AlertFeedItemsController < ApplicationController
 
   def index
     region = Region.find_by(region_identifier: params[:region_id])
-    @items = region.alert_feed_items.includes(:alert_feed).where(condition_filters(params)).limit(20)
+    @items = if region.nil?
+      []
+    else
+      region.alert_feed_items.includes(:alert_feed).where(condition_filters(params)).limit(20)
+    end
+    
     respond_to do |format|
       format.json
     end
