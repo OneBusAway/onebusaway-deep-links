00db83adbb21609d2fe4ba8866c30cca2ce2b9f5
Aaron Brethorst <aaron@brethorsting.com>
2017-12-28 09:57:00 -0800

    Clean up display

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -8,8 +8,7 @@ class StopsController < ApplicationController
   def trips
     logger.warn "*** BEGIN RAW REQUEST HEADERS ***"
     self.request.env.each do |header|
-      logger.warn "HEADER KEY: #{header[0]}"
-      logger.warn "HEADER VAL: #{header[1]}"
+      logger.warn "#{header[0]} == #{header[1]}"
     end
     logger.warn "*** END RAW REQUEST HEADERS ***"
     
