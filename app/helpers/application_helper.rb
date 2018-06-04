2d0cc8950bae8e34accff01d54356fc6fb930e16
Aaron Brethorst <aaron@brethorsting.com>
2018-06-04 09:27:10 -0700

    Wrap alert feed items list in a fluid container

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       @title = t
     end
   end
+  
+  def fluid_container(fluid = nil)
+    if fluid.nil?
+      @use_fluid_container || false
+    else
+      @use_fluid_container = fluid
+    end
+  end
 end
