faaf36fc4f1a6066956e7f94dbb3d9a66dc76a46
Aaron Brethorst <aaron@brethorsting.com>
2018-06-25 11:11:36 -0700

    Removes JSON output from alerts API

--- app/controllers/api/v1/alerts_controller.rb
+++ app/controllers/api/v1/alerts_controller.rb
@@ -5,7 +5,6 @@ class Api::V1::AlertsController < Api::V1::ApiController
     pb_message = GtfsMapper.alerts_to_pb_message(alerts, Time.now.to_i)
         
     respond_to do |format|
-      format.json { render(json: pb_message.to_json) }
       format.pb { send_data(pb_message.serialize_to_string) }
       format.pbtext { render(plain: pb_message.text_format_to_string) }
     end
