98e7dae593948fcc1e737cff7a2d44b3b1691296
Aaron Brethorst <aaron@brethorsting.com>
2018-06-25 21:48:06 -0700

    Shortens the recoded Sound Transit feed output to speed up tests
    The old VCR cassette file contained about 3.5MB of data that needed to be deserialized on every test invocation, which took about 7 seconds on my laptop. This new VCR cassette file loads in a fraction of the time.

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -3,4 +3,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'vcr_cassettes'
   c.hook_into :webmock
+  c.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
+  end
 end
