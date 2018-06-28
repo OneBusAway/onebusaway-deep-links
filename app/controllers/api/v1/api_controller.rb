3608ea36a31c6978f1f9cac424f5565fe0e61b01
Aaron Brethorst <aaron@brethorsting.com>
2018-06-27 21:43:30 -0700

    Generates a 404 error in response to a bad region ID

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :load_region
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   
   protected
   
   def load_region
     @region = Region.find_by!(region_identifier: params[:region_id])
+  end
+  
+  def render_not_found_response(ex)
+    render json: { error: ex.message }, status: :not_found
   end
 end
--- spec/requests/alarms_spec.rb
+++ spec/requests/alarms_spec.rb
@@ -5,7 +5,23 @@ require 'rails_helper'
 describe Api::V1::AlarmsController, type: :request do
   
   describe "POST /api/v1/regions/:id/alarms" do
-    skip
+    context 'with a bad region ID' do
+      before do
+        post("/api/v1/regions/BAD_ID/alarms", params: {})
+      end
+      
+      it 'returns status 404' do
+        expect(response.status).to eq(404)
+      end
+      
+      it 'has a json body with an error message' do
+        expect(json).to eq({ "error" => "Couldn't find Region" })
+      end
+    end
+    
+    context 'with a well formed request' do
+      xit
+    end
   end
   
   describe "DELETE /api/v1/regions/:region_id/alarms/:id" do
