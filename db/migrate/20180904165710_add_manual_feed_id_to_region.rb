e04bf12daf80498c45a1679281c68604b10ecabd
Aaron Brethorst <aaron@brethorsting.com>
2018-09-05 22:24:13 -0700

    Ugh, we'll do it manually then.

--- db/migrate/20180904165710_add_manual_feed_id_to_region.rb
+++ db/migrate/20180904165710_add_manual_feed_id_to_region.rb
@@ -2,12 +2,11 @@ class AddManualFeedIdToRegion < ActiveRecord::Migration[5.2]
   def change
     add_column :regions, :manual_feed_id, :integer
     add_index :regions, :manual_feed_id
-    
-    Region.reset_column_information
-    
-    puget_sound = Region.find_by(region_identifier: 1)
-    manual_feed = puget_sound.alert_feeds.find_by(type: 'PugetSoundManualAlertFeed')
-    puget_sound.manual_feed = manual_feed
-    puget_sound.save!
+
+    # Do this manually, I guess!
+    # puget_sound = Region.find_by(region_identifier: 1)
+    # manual_feed = puget_sound.alert_feeds.find_by(type: 'PugetSoundManualAlertFeed')
+    # puget_sound.manual_feed = manual_feed
+    # puget_sound.save!
   end
 end
