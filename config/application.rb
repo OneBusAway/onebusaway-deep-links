e1bfdaad43704e1bacd9c17a477ef22f2a82d68f
Aaron Brethorst <aaron@brethorsting.com>
2018-10-15 09:52:48 -0700

    Moves alert feed models back to main models directory

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,5 @@ module Obaco
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.watchable_dirs['lib'] = [:rb]
-    config.autoload_paths += Dir[Rails.root.join("app", "models", "alert_feeds")]
   end
 end
