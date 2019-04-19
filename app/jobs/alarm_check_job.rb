7558c013bff4c38f1e17bfc908f14729273f88fb
Aaron Brethorst <aaron@brethorsting.com>
2019-04-19 00:36:30 -0700

    Fixes a bug that I should have caught with a test. doh.

--- app/jobs/alarm_check_job.rb
+++ app/jobs/alarm_check_job.rb
@@ -3,6 +3,7 @@ class AlarmCheckJob < ApplicationJob
 
   def perform(alarm_id)
     puts "Checking alarm #{alarm_id}."
-    AlarmChecker.check_alarm(alarm_id)
+    checker = AlarmChecker.new(id: alarm_id)
+    checker.check_alarm()
   end
 end
