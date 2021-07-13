673e05edac5969f7bc97627a705ef7b8ea0e513b
Aaron Brethorst <aaron@brethorsting.com>
2021-07-12 23:54:06 -0500

    Try to fix my weird, busted syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,11 +57,9 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = (
-    :mem_cache_store,
+  config.cache_store = :mem_cache_store,
     (ENV["MEMCACHEDCLOUD_SERVERS"] || "").split(','),
     { username: ENV["MEMCACHEDCLOUD_USERNAME"], password: ENV["MEMCACHEDCLOUD_PASSWORD"] }
-  )
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :sidekiq
