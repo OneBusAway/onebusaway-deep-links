3be4ce81bfc9964b348641fbd010d83d30ded3b1
Aaron Brethorst <aaron@brethorsting.com>
2021-07-12 23:16:48 -0500

    Removes Raygun initializer

--- config/initializers/raygun.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.production?
-  Raygun.setup do |config|
-    config.api_key = ENV['RAYGUN_APIKEY']
-    config.filter_parameters = Rails.application.config.filter_parameters
-
-    # The default is Rails.env.production?
-    # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
-  end
-end
