96dca3a5195d11f0de14bd1b4db18a0188202c99
Aaron Brethorst <aaron@brethorsting.com>
2021-07-12 23:52:11 -0500

    Removes :dalli_store
    It is causing issues with caching requests for OBA API calls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,11 +57,11 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store
-  if ENV["MEMCACHEDCLOUD_SERVERS"]
-    creds = { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
-    config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), creds
-  end
+  config.cache_store = (
+    :mem_cache_store,
+    (ENV["MEMCACHEDCLOUD_SERVERS"] || "").split(','),
+    { username: ENV["MEMCACHEDCLOUD_USERNAME"], password: ENV["MEMCACHEDCLOUD_PASSWORD"] }
+  )
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :sidekiq
