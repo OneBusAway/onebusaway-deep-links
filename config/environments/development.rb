0c217c044183ca68d4e0dffba097421ac2c1ed50
Aaron Brethorst <aaron@brethorsting.com>
2023-11-20 12:45:48 -0800

    Try to configure ngrok allowlisting again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "*.ngrok-free.app"
+  config.hosts << %r{.+\.ngrok-free.app$}
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
