e3baffb6bf6ed0c52f765d397ddb485c183a80bc
Aaron Brethorst <aaron@brethorsting.com>
2024-01-05 14:04:05 -0800

    Reduce Redis usage

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,9 +1,11 @@
 module StatsHelper
   def record_pageview(region, page)
-    @page_counter = Redis::Counter.new("#{region.region_identifier}:#{page}:#{DateTime.now.strftime("%Y-%m-%d")}")
-    @page_counter.increment
-    
-    @api_counter = Redis::Counter.new("#{region.region_identifier}:api:#{DateTime.now.strftime("%Y-%m-%d")}")
-    @api_counter.increment    
+    # disabling this feature for now. I have never looked at it and it's blowing up our Redis usage.
+
+    # @page_counter = Redis::Counter.new("#{region.region_identifier}:#{page}:#{DateTime.now.strftime("%Y-%m-%d")}")
+    # @page_counter.increment
+    #
+    # @api_counter = Redis::Counter.new("#{region.region_identifier}:api:#{DateTime.now.strftime("%Y-%m-%d")}")
+    # @api_counter.increment
   end
 end
