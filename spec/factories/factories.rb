c031bebf922391526f3cd5fdc11ee271a2282656
Aaron Brethorst <aaron@brethorsting.com>
2024-06-30 22:45:48 -0700

    Attempt to work around a weird deployment issue

---
+++ spec/factories/factories.rb
@@ -0,0 +1,31 @@
+FactoryBot.define do
+  factory :admin do
+    name { "Admin" }
+    email { "admin@example.com" }
+    password { "password" }
+    region
+  end
+
+  factory :region do
+    sequence(:region_identifier) { |n| n }
+    sequence(:api_url) { |n| "https://example.org/#{n}/" }
+    sequence(:web_url) { |n| "https://example.com/#{n}/" }
+    name { "Example Region" }
+  end
+
+  factory :study do
+    name { "Study" }
+    description { "Study Description" }
+    region
+  end
+
+  factory :survey do
+    name { "Survey Name" }
+    study
+    available { false }
+  end
+
+  factory :question do
+    survey
+  end
+end
