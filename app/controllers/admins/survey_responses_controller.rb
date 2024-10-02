c0898bc0a8e9cd9eeaac0c6e068d658150ace4b6
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-02 17:12:17 +0300

    feat: Add CSV generation and download functionality

--- app/controllers/admins/survey_responses_controller.rb
+++ app/controllers/admins/survey_responses_controller.rb
@@ -4,6 +4,14 @@ class Admins::SurveyResponsesController < ApplicationController
 
   def index
     @survey_responses = @survey.survey_responses.order(:created_at)
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        filename = generate_filename(@survey)
+        send_data SurveyResponse.to_csv(@survey_responses), filename:, content_type: 'text/csv'
+      end
+    end
   end
 
   def show
@@ -14,5 +22,13 @@ class Admins::SurveyResponsesController < ApplicationController
 
   def load_resources
     @survey = current_admin_region.surveys.find(params[:survey_id])
+  end
+
+  # Helper method to generate CSV filename
+  def generate_filename(survey)
+    study_name = survey.study.name.parameterize(separator: '_')
+    survey_name = survey.name.parameterize(separator: '_')
+    date = Date.today.to_s
+    "study_#{study_name}_survey_#{survey_name}_responses_#{date}.csv"
   end
 end
