f65ed89b38eed1a046e3c86d02dcbaf9732ee2c9
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-03 01:50:51 +0000

    chore: linting and formatting

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -3,7 +3,7 @@ module Filterable
 
   module ClassMethods
     def filter(filtering_params)
-      results = self.where(nil)
+      results = where(nil)
       filtering_params.each do |key, value|
         results = results.public_send("filter_by_#{key}", *value) if value.present?
       end
