8486053d2fc17f402e8e1b09288a9882d397f69f
Ahmedhossam <99441866+Ahmedhossamdev@users.noreply.github.com>
2024-10-13 22:51:35 +0300

    Add start_date and end_date to surveys (#179)
    Fixes #175 
    
    * feat: Add start_date and end_date fields
    * feat: Add filter_currently_active scope to filter by date
    * feat: Add validation for start and end date fields
    * feat: Add start_date and end_date fields to survey form
    * feat: Add tests for start and end date fields
    * feat: Add start_date and end_date columns to surveys table
    * feat: Add start_date and end_date fields to json builder
    * feat: Add start and end date fields to survey details view
    * refactor: title to use "Survey" instead of "Study"
    * refactor surveys_controller.rb to remove unnecessary comment from linting
    * feat: Add validation to ensure only currently active surveys are included in API output
    * refactor: improve validation in Survey model
    * feat: new date component form
    * refactor:  Used the new date time field component
    * refactor: Improve validation in Survey model

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,9 +1,25 @@
-require 'rails_helper'
-
 RSpec.describe Survey, type: :model do
   it 'requires a name' do
     survey = Survey.new
-    survey.valid?
+    expect(survey).to_not be_valid
     expect(survey.errors[:name]).to include("can't be blank")
+  end
+
+  it 'requires a start_date if end_date is provided' do
+    survey = Survey.new(end_date: Time.current + 1.day)
+    expect(survey).to_not be_valid
+    expect(survey.errors[:start_date]).to include("must be present if end_date is provided")
+  end
+
+  it 'requires an end_date if start_date is provided' do
+    survey = Survey.new(start_date: Time.current)
+    expect(survey).to_not be_valid
+    expect(survey.errors[:end_date]).to include("must be present if start_date is provided")
+  end
+
+  it 'validates that end_date must be after start_date' do
+    survey = Survey.new(start_date: Time.current, end_date: Time.current - 1.day)
+    expect(survey).to_not be_valid
+    expect(survey.errors[:end_date]).to include("must be after the start date")
   end
 end
