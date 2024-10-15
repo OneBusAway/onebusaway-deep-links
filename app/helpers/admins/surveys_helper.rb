fa601ca97b37d419e87391ebb7e3a75cf59140b8
Amr Hossam <amrhossam.dev@gmail.com>
2024-10-15 06:19:37 +0300

    improve visible note messages && visible route/stops list

--- app/helpers/admins/surveys_helper.rb
+++ app/helpers/admins/surveys_helper.rb
@@ -11,18 +11,38 @@ module Admins::SurveysHelper
   # @param survey [Survey]
   # @return [String] the visibility of the survey
   def survey_visibility(survey)
-    if survey.show_on_map && survey.show_on_stops
-      return "Visible on specific stops and routes" if survey.visible_stop_list.present? && survey.visible_route_list.present?
+    return "Not visible" unless survey.show_on_map || survey.show_on_stops
+
+    if survey.show_on_map
+      handle_map_visibility(survey)
+    elsif survey.show_on_stops
+      handle_stop_visibility(survey)
+    end
+  end
+
+  private
+
+  def handle_map_visibility(survey)
+    if survey.show_on_stops
+      return "Visible on specific stops, routes and map" if survey.visible_stop_list.present? && survey.visible_route_list.present?
       return "Visible on specific stops and map" if survey.visible_stop_list.present?
       return "Visible on specific routes and map" if survey.visible_route_list.present?
 
       "Visible on all stops and map"
-    elsif survey.show_on_stops
-      "Visible on all stops"
-    elsif survey.show_on_map
-      "Visible on map"
     else
-      "Not visible"
+      "Visible on map"
+    end
+  end
+
+  def handle_stop_visibility(survey)
+    if survey.visible_stop_list.present?
+      return "Visible on specific stops and routes" if survey.visible_route_list.present?
+
+      "Visible on specific stops"
+    elsif survey.visible_route_list.present?
+      "Visible on specific routes"
+    else
+      "Visible on all stops"
     end
   end
 end
