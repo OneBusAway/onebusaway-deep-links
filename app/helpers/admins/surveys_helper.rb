22754d3e61f96e45e1a45ced4a8b9619108c15a7
Amr Hossam <amrhossam.dev@gmail.com>
2024-10-15 03:48:29 +0300

    feat: add survey visibility for better ux

--- app/helpers/admins/surveys_helper.rb
+++ app/helpers/admins/surveys_helper.rb
@@ -6,4 +6,23 @@ module Admins::SurveysHelper
       ["SurveyMonkey", "survey_monkey"]
     ]
   end
+
+  # Return a string describing visibility of the survey
+  # @param survey [Survey]
+  # @return [String] the visibility of the survey
+  def survey_visibility(survey)
+    if survey.show_on_map && survey.show_on_stops
+      return "Visible on specific stops and routes" if survey.visible_stop_list.present? && survey.visible_route_list.present?
+      return "Visible on specific stops and map" if survey.visible_stop_list.present?
+      return "Visible on specific routes and map" if survey.visible_route_list.present?
+
+      "Visible on all stops and map"
+    elsif survey.show_on_stops
+      "Visible on all stops"
+    elsif survey.show_on_map
+      "Visible on map"
+    else
+      "Not visible"
+    end
+  end
 end
