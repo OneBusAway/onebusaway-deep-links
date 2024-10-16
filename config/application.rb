3eaada10c05141a041ce57a9becc92f01461a994
Aaron Brethorst <aaron@brethorsting.com>
2024-10-15 18:19:52 -0700

    Try to fix Tailwind problem in production

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Obaco2
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.assets.css_compressor = nil
 
     # Configuration for the application, engines, and railties goes here.
     #
