46f0ccaf0fc08b12230e7784c2be61cd054eefff
Amr Hossam <amrhossam.dev@gmail.com>
2024-10-16 06:56:57 +0300

    Surveys: Improve UX for survey visibility options (#217)
    Fixes: #177 and #216
    
    * feat: add survey visibility for better ux
    * improve visible note messages && visible route/stops list
    * Better place for visibility settings
    * Disable routes and stop list when show on stop disabled

---
+++ app/javascript/controllers/toggle_stop_route_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    static targets = ["showOnStopsCheckbox", "visibleStopList", "visibleRouteList"];
+
+    connect() {
+        this.toggleStopAndRouteLists();
+    }
+
+    toggleStopAndRouteLists() {
+        const isDisabled = !this.showOnStopsCheckboxTarget.checked;
+        this.visibleStopListTarget.disabled = isDisabled;
+        this.visibleRouteListTarget.disabled = isDisabled;
+
+        this.visibleStopListTarget.style.opacity = isDisabled ? '0.5' : '1';
+        this.visibleRouteListTarget.style.opacity = isDisabled ? '0.5' : '1';
+    }
+
+    toggle() {
+        this.toggleStopAndRouteLists();
+    }
+}
